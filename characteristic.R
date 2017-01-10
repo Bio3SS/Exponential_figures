46db24b6d27e42a35bb8e5a69661d4efb984697d
Jonathan Dushoff <dushoff@mcmaster.ca>
2017-01-09 22:22:15 -0500

    Fiddling with   characteristic.R

--- characteristic.R
+++ characteristic.R
@@ -1,5 +1,5 @@
-time <- seq(0, 20, length.out=101)
-Tc <- -6
+time <- seq(0, 6, length.out=101)
+Tc <- -1
 N0 <- 100
 
 N <- exp(time/Tc)
@@ -9,6 +9,7 @@ par (cex=1.5)
 LineFun <- function(t, Tc){
 	x <- c(t, t-Tc)
 	y <- c(exp(t/Tc), 0)
+	points(x[[1]], y[[1]], col="blue", pch=20)
 	lines(x, y, col="blue")
 }
 
@@ -16,7 +17,6 @@ plot(time, N
 	, type = "l"
 )
 
-LineFun(4, Tc)
-LineFun(8, Tc)
-LineFun(12, Tc)
-LineFun(16, Tc)
+LineFun(0.5, Tc)
+LineFun(1.5, Tc)
+LineFun(2.5, Tc)
